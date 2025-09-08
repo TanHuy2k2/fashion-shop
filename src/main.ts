9c112bd14cd621054d73bd71433beeca2d97c433
TanHuy652k2 <tanhuyqn6789@gmail.com>
2025-09-08 09:11:44 +0700

    fashion shop

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { engine } from 'express-handlebars';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.engine('hbs', engine({
+    extname: '.hbs',
+    defaultLayout: 'main',
+    layoutsDir: join(__dirname, '..', 'views', 'layouts'),
+    partialsDir: join(__dirname, '..', 'views', 'partials'),
+  }));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
